57cdecbf47eaf5218dcb5640855e6aa69b705493
Mikhail Letunovskiy <letunovskiymn@korablil.ru>
2018-01-15 10:33:09 +0300

    add log file

--- index.php
+++ index.php
@@ -26,15 +26,19 @@ $response = $client->request('GET', '/resumes/mine', $headers);
 if ($response->getStatusCode() == 200){
     $data=json_decode($response->getBody(),true);
     foreach ($data['items'] as $resume){
+        $date=new DateTime();
         $idResume=$resume['id'];
         try {
             $responseResume = $client->request('POST', '/resumes/'.$idResume.'/publish', $headers);
             if ($responseResume->getStatusCode()==204){
                 //very good
+                file_put_contents('log.txt',$date->format('d-m-Y H:s:i')."\n".
+                    $responseResume->getBody()."\n", FILE_APPEND);
             }
         }
         catch (ClientException $e){
-
+            file_put_contents('log.txt',$date->format('d-m-Y H:s:i')."\n".
+                $e->getMessage()."\n", FILE_APPEND);
         }
     }
 }
